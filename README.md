38c1cc1832c13e6deee8d9a0f270c84eeb08a691
Akos Kemives <akoskm@gmail.com>
2016-08-02 23:20:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Material-UI](http://callemall.github.io/material-ui/) - Example Webpack Project
+
+This is an example project that uses [Material-UI](http://callemall.github.io/material-ui/) an based on [material-ui-webpack-example](https://github.com/callemall/material-ui-webpack-example/tree/4dba4758a56756921e2ec75b352c41d1325a6241).
---
+++ src/app/Main.js
@@ -0,0 +1,123 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+import React, {Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import AppBar from 'material-ui/AppBar';
+import AppNavDrawer from './AppNavDrawer';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+  appBar: {
+    position: 'fixed',
+    top: 0,
+  }
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class Main extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleTouchTapLeftIconButton = this.handleTouchTapLeftIconButton.bind(this);
+    this.handleChangeRequestNavDrawer = this.handleChangeRequestNavDrawer.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+
+    this.state = {
+      open: false,
+      navDrawerOpen: false
+    };
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  handleTouchTapLeftIconButton() {
+    this.setState({
+      navDrawerOpen: !this.state.navDrawerOpen,
+    });
+  }
+
+  handleChangeRequestNavDrawer(open) {
+    this.setState({
+      navDrawerOpen: open,
+    });
+  }
+
+  render() {
+    styles.navDrawer = {
+        zIndex: styles.appBar.zIndex - 1,
+    };
+    let docked = false;
+    let navDrawerOpen = this.state.navDrawerOpen;
+    const standardActions = (
+      <FlatButton
+        label="Ok"
+        primary={true}
+        onTouchTap={this.handleRequestClose}
+      />
+    );
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={styles.container}>
+          <Dialog
+            open={this.state.open}
+            title="Super Secret Password"
+            actions={standardActions}
+            onRequestClose={this.handleRequestClose}
+          >
+            1-2-3-4-5
+          </Dialog>
+          <AppBar
+            title="KTS Client"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.handleTouchTapLeftIconButton}
+            style={styles.appBar}
+          />
+          <AppNavDrawer
+            style={styles.navDrawer}
+            location={location}
+            docked={docked}
+            onRequestChangeNavDrawer={this.handleChangeRequestNavDrawer}
+            onChangeList={this.handleChangeList}
+            open={navDrawerOpen}
+          />
+          <h1>Material-UI</h1>
+          <h2>example project</h2>
+          <RaisedButton
+            label="Super Secret Password"
+            secondary={true}
+            onTouchTap={this.handleTouchTap}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Main;
