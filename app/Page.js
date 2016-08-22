e360e916ee46a6e2bffc873ca7201967de94aaba
Akos Kemives <akoskm@gmail.com>
2016-08-22 14:41:52 +0200

    initial page layout

--- app/Page.js
+++ app/Page.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const styles = {
+  appBar: {
+    position: 'fixed',
+    top: 0,
+    margin: '0px 0px 0px -8px'
+  }
+};
 
 class Page extends React.Component {
 
@@ -7,8 +16,25 @@ class Page extends React.Component {
   }
 
   render() {
-    return (<div>Page</div>);
+    let nameslug = '';
+    if (this.props.routeParams) {
+      nameslug = this.props.routeParams.nameslug;
+    }
+    return (
+      <div>
+        <AppBar
+            title={nameslug}
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.handleTouchTapLeftIconButton}
+            style={styles.appBar}
+          />
+      </div>
+    );
   }
 }
+
+Page.propTypes = {
+  routeParams: React.PropTypes.object.isRequired
+};
 
 export default Page;
